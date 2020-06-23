0c5751fc37176ed5815e7d1138e6d2a52e2956f0
tongplw <bangkok.tong@gmail.com>
2020-06-23 11:35:43 +0700

    add threading to muse

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import time
 from muse import MuseMonitor
 
 
-SERVER = "192.168.43.126"
+SERVER = "192.168.1.122"
 PORT = 5000
 
 if __name__ == "__main__":
     muse = MuseMonitor(SERVER, PORT)
-    muse.run()
+    
+    while True:
+        print(muse.raw)
+        print(muse.waves)
+        print(muse.attention)
+        time.sleep(1)
--- muse.py
+++ muse.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+from threading import Thread
 from datetime import datetime
 from pythonosc import dispatcher, osc_server
 
@@ -10,17 +11,24 @@ class MuseMonitor():
     def __init__(self, server, port):
         self.server = server
         self.port = port
-        self.buffer = []
         self.fft_window_size = 1024
         self.sample_rate = 256
-        self.attention_buff = [50, 50, 50, 50, 50]
-    
+        self._buffer = []
+        self._attention_buff = [50, 50, 50, 50, 50]
+        self.raw = 0
+        self.waves = {}
+        self.attention = 0
+
+        thread = Thread(target=self._run)
+        thread.daemon = True
+        thread.start()
+
     def _get_dispatcher(self):
         d = dispatcher.Dispatcher()
         d.map("/debug", print)
         d.map("/muse/eeg", self._eeg_handler, "EEG")
         return d
-    
+
     def _get_fft(self, raw_list):
         fft = np.abs(np.fft.rfft(raw_list))
         freqs = np.fft.rfftfreq(self.fft_window_size, 1 / self.sample_rate)
@@ -40,7 +48,8 @@ class MuseMonitor():
             band_list[b] = np.mean(band_list[b])
         return band_list
 
-    def _attention(self, power_band):
+    def _attention(self, waves):
+        waves = waves.copy()
         index = ['delta', 'theta', 'low-alpha', 'high-alpha', 'low-beta', 'high-beta',
                 'low-gamma', 'mid-gamma', 'attention-1', 'attention-2', 'attention-3',
                 'attention-4', 'attention-5', 'log2-delta', 'log2-theta',
@@ -57,28 +66,28 @@ class MuseMonitor():
                 -3.90226910e-03, -4.99784235e-03, -7.18270207e-01]
 
         for i in range(5):
-            power_band[f'attention-{i+1}'] = self.attention_buff[i]
-        for i in list(power_band):
-            power_band[f'log2-{i}'] = np.log2(power_band[i])
-        power_band['log2-theta-alpha'] = np.log2(power_band['theta'] + power_band['low-alpha'] + power_band['high-alpha'])
+            waves[f'attention-{i+1}'] = self._attention_buff[i]
+        for i in list(waves):
+            waves[f'log2-{i}'] = np.log2(waves[i])
+        waves['log2-theta-alpha'] = np.log2(waves['theta'] + waves['low-alpha'] + waves['high-alpha'])
 
         att = 0
         for ind, coe in zip(index, coef):
-            att += power_band[ind] * coe
-        self.attention_buff = [max(1e-5, att)] + self.attention_buff[:-1]
+            att += waves[ind] * coe
+        self._attention_buff = [max(1e-5, att)] + self._attention_buff[:-1]
         return att
-        
+
     def _eeg_handler(self, unused_addr, args, TP9, AF7, AF8, TP10, AUX):
-        raw = AF7 - TP9 # TP9 หลังหู-ซ้าย, AF7 หน้าผาก-ซ้าย
-        self.buffer.append(raw)
-        if len(self.buffer) > self.fft_window_size:
-            self.buffer = self.buffer[1:]
+        self.raw = AF7 - TP9 # TP9 หลังหู-ซ้าย, AF7 หน้าผาก-ซ้าย
+        self._buffer.append(self.raw)
+        if len(self._buffer) > self.fft_window_size:
+            self._buffer = self._buffer[1:]
 
-            power_band = self._get_bands(self.buffer)
-            print(self._attention(power_band))
-            self.buffer = self.buffer[self.sample_rate:]
+            self.waves = self._get_bands(self._buffer)
+            self.attention = self._attention(self.waves)
+            self._buffer = self._buffer[self.sample_rate:]
 
-    def run(self):
+    def _run(self):
         server = osc_server.BlockingOSCUDPServer((self.server, self.port), self._get_dispatcher())
         try:
             server.serve_forever()
